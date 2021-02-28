a7e3bc7099cf5adaed5047019e9700a6b4a1eb29
bufffman <staduxin1999@mail.ru>
2021-03-01 00:49:23 +0700

    improved the filter

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
-<body>
+<body id="body">
 	<!-- Content here -->
 	<div class="wrapper">
 		<div class="row justify-content-start">
@@ -337,24 +337,33 @@
 		// 		modal.style.display = "none";
 		// 	}
 		// }
-
+		
 		let mybtn = document.getElementById('myBtn');
 		let closeModal = document.querySelector('.close-modal');
-
+		
+		// When click on button > show modal window (filter)
 		mybtn.addEventListener('click', function() {
 			document.getElementById('myModal').classList.add('showModal');
 		});
 
-		// closeModal.addEventListener('click', function() {
-		// 	document.getElementById('myModal').classList.remove('showModal');
-		// });
+		// When click on button > body dont have scroll
+		mybtn.addEventListener('click', function() {
+			document.getElementById('body').classList.add('modal-open-hidden-scroll');
+		});
 
+		// When click on button > class whith delete show modal, removes
 		mybtn.addEventListener('click', function() {
 		 	document.getElementById('myModal').classList.remove('notShowModal');
 		 });
 
+		 // When click on close button > modal window get class, with give show
 		closeModal.addEventListener('click', function() {
 			document.getElementById('myModal').classList.add('notShowModal');
+		});
+
+		// When click on close button > modal window remove class, with delete scroll in body
+		closeModal.addEventListener('click', function() {
+			document.getElementById('body').classList.remove('modal-open-hidden-scroll');
 		});
 		
 	</script>
